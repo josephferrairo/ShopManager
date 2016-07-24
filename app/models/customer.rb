ac8f35de95293e78889cc2c50e4e1a3f8ec39dfb
josephferrairo <josephferrairo@gmail.com>
2016-07-24 13:43:21 -0400

    added model tests for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  validates :name, :custuomercode, :address, :city, :state, :zipcode, presence: true
+  validates :name, :customercode, :address, :city, :state, :zipcode, presence: true
   validates :state, length: { is: 2 }
   validates :zipcode, presence: true, length: { is: 5 }
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:customercode) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_length_of(:state).is_equal_to(2) }
+  it { should validate_presence_of(:zipcode) }
+  it { should validate_length_of(:zipcode).is_equal_to(5) }
+
 end
