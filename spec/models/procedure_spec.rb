de1e8fa64a03b499238443803757cc6a5a6c2ef6
josephferrairo <josephferrairo@gmail.com>
2016-07-24 13:21:08 -0400

    added model tests for procedure

---
+++ spec/models/procedure_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Procedure, type: :model do
+  it { should have_many(:work_instructions) }
+  it { should have_many(:procedure_work_instructions) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:procedurecode) }
+  it { should validate_length_of(:procedurecode) }
+end
