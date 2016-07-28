575e2c1cfe2996847958622a2fb5299fd878e99e
josephferrairo <josephferrairo@gmail.com>
2016-07-27 22:10:41 -0400

    part model setup

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,8 +1,6 @@
 class Part < ApplicationRecord
   belongs_to :customer
   belongs_to :procedure
-
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-
 end
