b67fe1de01b136588655425bf498c3156bd6733f
josephferrairo <josephferrairo@gmail.com>
2016-07-28 22:42:35 -0400

    added validate presence of procedure_id

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,6 +1,6 @@
 class Part < ApplicationRecord
   belongs_to :customer
   belongs_to :procedure
-  validates :name, :description, :price, presence: true
+  validates :name, :description, :price, :procedure_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
 end
