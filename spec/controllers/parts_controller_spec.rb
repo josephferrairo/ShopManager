25ef735ab526d4be376203899c7e7c5056214752
josephferrairo <josephferrairo@gmail.com>
2016-08-03 18:18:29 -0400

    refactored customer_before method

--- spec/controllers/parts_controller_spec.rb
+++ spec/controllers/parts_controller_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PartsController, type: :controller do
-
-  describe "get #new" do
+  def customer_before
     before(:each) do
       @customer = Fabricate(:customer)
     end
-    it "returns a successful http request status code" do
-      get :new, :customer_id => @customer
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe "POST #create" do
-    before(:each) do
-      @customer = Fabricate(:customer)
+    describe "get #new" do
+      customer_before
+      it "returns a successful http request status code" do
+        get :new, :customer_id => @customer
+        expect(response).to have_http_status(:success)
+      end
     end
-    context "a successful create" do
-      it "saves a new part object" do
-        post :create, :customer_id => @customer,
-        part: Fabricate.attributes_for(:part)
-        expect(Part.count).to eq(1)
+
+    describe "POST #create" do
+      customer_before
+      context "a successful create" do
+        it "saves a new part object" do
+          post :create, :customer_id => @customer,
+          part: Fabricate.attributes_for(:part)
+          expect(Part.count).to eq(1)
+        end
       end
     end
   end
-
 end
