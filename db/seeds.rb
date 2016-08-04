51d3476417e89eb9d189aba8a5c44e8cd3869ee6
josephferrairo <josephferrairo@gmail.com>
2016-08-03 21:55:03 -0400

    added seed data for customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+task :sample_data => :environment do
+  require 'faker'
+end
+
+def create_customer
+  20.times do |n|
+    name = Faker::Company.name
+    customercode = ([*('A'..'Z'),*('0'..'9')].sample(4).join)
+    address = Faker::Address.city
+    addressdetails = Faker::Address.secondary_address
+    city = Faker::Address.city
+    state = Faker::Address.state_abbr
+    zipcode = Faker::Number.number(5)
+    Customer.create!(
+    name: name,
+    customercode: customercode,
+    address: address,
+    addressdetails: addressdetails,
+    city: city,
+    state: state,
+    zipcode: zipcode
+    )
+  end
+end
+
+create_customer
