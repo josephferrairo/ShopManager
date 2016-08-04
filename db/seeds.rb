8a4f7a741fb35cd3af5c55f015eb03fcd200ac59
josephferrairo <josephferrairo@gmail.com>
2016-08-03 22:20:23 -0400

    added work instructions seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ task :sample_data => :environment do
   require 'faker'
 end
 
-def create_customer
+def create_customers
   20.times do |n|
     name = Faker::Company.name
     customercode = ([*('A'..'Z'),*('0'..'9')].sample(4).join)
@@ -12,15 +12,29 @@ def create_customer
     state = Faker::Address.state_abbr
     zipcode = Faker::Number.number(5)
     Customer.create!(
-    name: name,
-    customercode: customercode,
-    address: address,
-    addressdetails: addressdetails,
-    city: city,
-    state: state,
-    zipcode: zipcode
+     name: name,
+     customercode: customercode,
+     address: address,
+     addressdetails: addressdetails,
+     city: city,
+     state: state,
+     zipcode: zipcode
     )
   end
 end
 
-create_customer
+def create_work_instructions
+  20.times do |n|
+    name = Faker::Superhero.name
+    workcode = ([*('A'..'Z'),*('0'..'9')].sample(4).join)
+    description = Faker::StarWars.quote
+    WorkInstruction.create!(
+     name: name,
+     workcode: workcode,
+     description: description
+    )
+  end
+end
+
+create_customers
+create_work_instructions
