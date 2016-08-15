7b9f71b051de2053fb1ecc24f26e0e17fe43a35d
josephferrairo <josephferrairo@gmail.com>
2016-08-14 20:31:09 -0400

    added part update, edit, delete, and tests

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -8,12 +8,37 @@ class PartsController < ApplicationController
   def create
     @customer = Customer.find(params[:customer_id])
     @part = @customer.parts.create(part_params)
-    if @part.valid?
+    if @part.save
       flash[:success] = 'New Part has been created!'
       redirect_to customer_path(@customer)
     else
       render :new, :status => :unprocessable_entity
     end
+  end
+
+  def edit
+    @customer = Customer.find(params[:customer_id])
+    @part = @customer.parts.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:customer_id])
+    @part = @customer.parts.find(params[:id])
+    @part.update_attributes(part_params)
+    if @part.save
+      flash[:success] = 'Part has been updated'
+      redirect_to customers_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:customer_id])
+    @part = @customer.parts.find(params[:id])
+    @part.destroy
+    flash[:error] = 'Part has been deleted!'
+    redirect_to customers_path
   end
 
   private
--- spec/controllers/parts_controller_spec.rb
+++ spec/controllers/parts_controller_spec.rb
@@ -1,25 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe PartsController, type: :controller do
-  def customer_before
-    before(:each) do
-      @customer = Fabricate(:customer)
-    end
+  before(:each) do
+    @customer = Fabricate(:customer)
+    @part = Fabricate(:part, customer: @customer)
+    @part_attributes = Fabricate.attributes_for(:part)
+  end
 
-    describe "get #new" do
-      it "returns a successful http request status code" do
-        get :new, :customer_id => @customer
-        expect(response).to have_http_status(:success)
+  describe "get #new" do
+    it "returns a successful http request status code" do
+      get :new, customer_id: @customer
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    context "a successful create" do
+      it "saves a new part object" do
+        expect { post :create, customer_id: @customer, part: @part_attributes }.to change( Part, :count).by(1)
       end
     end
+  end
 
-    describe "POST #create" do
-      context "a successful create" do
-        it "saves a new part object" do
-          post :create, :customer_id => @customer,
-          part: Fabricate.attributes_for(:part)
-          expect(Part.count).to eq(1)
-        end
+  describe '#DELETE destroy' do
+    it 'destroys the requested part' do
+      expect { delete :destroy, { customer_id:  @customer, id: @part.id } }.to change(Part, :count).by(-1)
+    end
+  end
+
+  describe 'Get #edit' do
+    let(:part) { @part }
+    it 'sends a successful edit request' do
+      get :edit, id: @part, customer_id: @customer
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'PUT #update' do
+    context "successful update" do
+      let(:abc) { Fabricate(:part, customer: @customer, name: 'abc') }
+      it 'updates the modified part object' do
+        put :update, customer_id: @customer, part: Fabricate.attributes_for(:part, name: 'xyz'), id: abc.id
+        expect(Part.last.name).to eq('xyz')
+        expect(Part.last.name).not_to eq('abc')
       end
     end
   end
